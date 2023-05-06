39d1f9a4a3b2e8c094728b25852b34eadb7b8562
LukaszCiaston-coder <lukasz.ciaston@interia.pl>
2023-05-06 11:48:54 +0200

    task 1 i 3

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+  const startButton = document.querySelector('[data-start]');
+  const stopButton = document.querySelector('[data-stop]');
 
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+  let intervalId = null;
+
+  startButton.addEventListener('click', function() {
+    startButton.disabled = true;
+    intervalId = setInterval(function() {
+      document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  });
+
+  stopButton.addEventListener('click', function() {
+    startButton.disabled = false;
+    clearInterval(intervalId);
+  });
